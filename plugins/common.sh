12c5ccd222ad130b712a169dc74296be4e425623
zf1976 <verticle@foxmail.com>
2022-02-25 00:52:17 +0800

    Update common.sh

--- plugins/common.sh
+++ plugins/common.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 
 # base
-opkg update && opkg install vim cfdisk git git-http usbutils pciutils
+opkg update && opkg install vim cfdisk git git-http usbutils pciutils lsof
 
 # luci-app
 opkg update && opkg install luci-app-statistics collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless collectd-mod-dns
