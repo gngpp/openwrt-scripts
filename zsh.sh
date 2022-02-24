e3afcfa8001db6d8f0a15cac668dfa38583f09bb
zf1976 <verticle@foxmail.com>
2022-02-24 12:44:24 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,7 +4,6 @@ opkg update && opkg install zsh wget unzip ca-certificates
 sh -c "$(wget -O- https://raw.githubusercontent.com/felix-fly/openwrt-ohmyzsh/master/install.sh)"
 
 which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
-source ~/.zshrc
 
 # zsh-syntax-highlighting
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
