85275f095ad19277149c10d67da4e3020fcb7c4d
zf1976 <verticle@foxmail.com>
2022-02-24 12:38:20 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -7,8 +7,10 @@ which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
 source ~/.zshrc
 
 # zsh-syntax-highlighting
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
+rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
