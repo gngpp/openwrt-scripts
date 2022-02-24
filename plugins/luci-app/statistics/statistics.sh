e480c9f01372dbe59fa0abde1f58544e4d00ba1a
zf1976 <1596863112@qq.com>
2022-02-24 14:00:40 +0800

    feat: update

---
+++ plugins/luci-app/statistics/statistics.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+opkg update && opkg install 
+opkg update && opkg install luci-app-statistics luci-i18n-statistics-zh-cn 
+opkg install collectd-mod-cpu collectd-mod-interface collectd-mod-memory collectd-mod-ping collectd-mod-rrdtool collectd-mod-wireless collectd-mod-dns
