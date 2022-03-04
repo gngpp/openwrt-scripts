3ec3f7c51a941383157c2b5b2709e4c9ff73ace7
zf1976 <verticle@foxmail.com>
2022-03-05 05:02:33 +0800

    Create aliyundrive-webdav.sh

---
+++ luci-app/statistics/aliyundrive-webdav.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+## r4s-arm64
+wget https://github.com/messense/aliyundrive-webdav/releases/download/v1.2.5/aliyundrive-webdav_1.2.5-1_aarch64_generic.ipk
+wget https://github.com/messense/aliyundrive-webdav/releases/download/v1.2.5/luci-app-aliyundrive-webdav_1.2.5_all.ipk
+wget https://github.com/messense/aliyundrive-webdav/releases/download/v1.2.5/luci-i18n-aliyundrive-webdav-zh-cn_1.2.5-1_all.ipk
+opkg install aliyundrive-webdav_1.2.5-1_aarch64_generic.ipk
+opkg install luci-app-aliyundrive-webdav_1.2.5_all.ipk
+opkg install luci-i18n-aliyundrive-webdav-zh-cn_1.2.5-1_all.ipk
