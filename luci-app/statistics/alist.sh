1eda8e1aebb4908b2cced36db405d34766e2ec20
zf1976 <verticle@foxmail.com>
2022-03-05 10:32:43 +0800

    Create alist.sh

---
+++ luci-app/statistics/alist.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+## r4s-arm64
+wget https://github.com/sbwml/openwrt-alist/releases/download/v2.1.0/alist_2.1.0-1_aarch64_generic.ipk
+wget https://github.com/sbwml/openwrt-alist/releases/download/v2.1.0/luci-app-alist_1.0.2_all.ipk
+wget https://github.com/sbwml/openwrt-alist/releases/download/v2.1.0/luci-i18n-alist-zh-cn_git-22.062.75936-0030bc0_all.ipk
+
+
+opkg install alist_2.1.0-1_aarch64_generic.ipk
+opkg install luci-app-alist_1.0.2_all.ipk
+opkg install luci-i18n-alist-zh-cn_git-22.062.75936-0030bc0_all.ipk
