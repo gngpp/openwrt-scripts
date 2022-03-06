49d20dc5f77d2b6b0501cd086723a16108bfc69d
Kardusen <verticle@foxmail.com>
2022-03-06 12:15:12 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,4 +1,4 @@
 #!/bin/ash
 
 # base
-opkg update && opkg install vim cfdisk git git-http usbutils pciutils lsof coreutils-nohup
+opkg update && opkg install vim rsync cfdisk git git-http usbutils pciutils lsof coreutils-nohup
