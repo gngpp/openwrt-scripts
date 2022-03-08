53e2b5cd07ffe3f24a411eff77c280a87aebd913
Kardusen <verticle@foxmail.com>
2022-03-08 23:02:59 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 opkg update && opkg install zsh
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
 
 which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
 source ~/.zshrc
