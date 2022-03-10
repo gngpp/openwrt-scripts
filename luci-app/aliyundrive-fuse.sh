25f6bcb53e48951dde3ab31b8de524d04dc155d4
Kardusen <verticle@foxmail.com>
2022-03-10 10:44:30 +0800

    Update aliyundrive-fuse.sh

--- luci-app/aliyundrive-fuse.sh
+++ luci-app/aliyundrive-fuse.sh
@@ -4,6 +4,7 @@ wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.7/aliyu
 wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.7/luci-app-aliyundrive-fuse_0.1.7_all.ipk
 wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.7/luci-i18n-aliyundrive-fuse-zh-cn_0.1.7-1_all.ipk
 
+opkg update
 opkg install aliyundrive-fuse_0.1.7-1_aarch64_generic.ipk
 opkg install luci-app-aliyundrive-fuse_0.1.7_all.ipk
 opkg install luci-i18n-aliyundrive-fuse-zh-cn_0.1.7-1_all.ipk
