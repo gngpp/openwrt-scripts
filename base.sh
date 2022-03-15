39b757096a5f64eef7f1ed11d89bd78b57a176c0
Kardusen <verticle@foxmail.com>
2022-03-15 13:44:12 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,3 +2,5 @@
 
 # base
 opkg update && opkg install vim rsync cfdisk git git-http usbutils pciutils lsof coreutils-nohup
+sed -i 's/"\?set number/set number/' /usr/share/vim/vimrc
+sed -i 's/"\?set rnu/set rnu/' /usr/share/vim/vimrc
