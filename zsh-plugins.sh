c6fe63fd6a109dadf3f1df8852184421ab12e76e
Kardusen <verticle@foxmail.com>
2022-03-16 19:17:50 +0800

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -7,4 +7,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+sed -i '73d' ~/.zshrc
 source ~/.zshrc
