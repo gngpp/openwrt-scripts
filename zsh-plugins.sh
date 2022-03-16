658f4a3ee35a933f08353099d5d2cf0fb180d9f3
Kardusen <verticle@foxmail.com>
2022-03-16 19:22:05 +0800

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # zsh-syntax-highlighting
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -8,4 +9,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 sed -i '73d' ~/.zshrc
+
+wget https://raw.githubusercontent.com/zf1976/packages/main/sysinfo.sh -P /etc
+echo "neofetch" >> ~/.zshrc
+echo "bash /etc/sysinfo.sh" >> ~/.zshrc
 source ~/.zshrc
+
+
