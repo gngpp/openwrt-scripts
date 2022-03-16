9775aec35218bdadfce032dc9aead284accb013c
Kardusen <verticle@foxmail.com>
2022-03-16 18:48:49 +0800

    Update nps-npc.sh

--- luci-app/nps-npc.sh
+++ luci-app/nps-npc.sh
@@ -10,4 +10,4 @@ opkg install npc_0.26.10-26_aarch64_generic.ipk nps_0.26.10-26_aarch64_generic.i
 rm ./luci-app-npc_git-22.027.50073-24f07fe_all.ipk
 rm ./luci-app-nps_git-22.049.56376-f644175_all.ipk
 rm ./nps_0.26.10-26_aarch64_generic.ipk
-rm ./install npc_0.26.10-26_aarch64_generic.ipk
+rm ./npc_0.26.10-26_aarch64_generic.ipk
