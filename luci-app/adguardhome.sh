f1e90363713f6d63d828a9c8662a536868f7c08a
Kardusen <verticle@foxmail.com>
2022-03-16 18:54:09 +0800

    Update adguardhome.sh

--- luci-app/adguardhome.sh
+++ luci-app/adguardhome.sh
@@ -4,4 +4,3 @@ wget https://op.supes.top/packages/aarch64_generic/luci-app-adguardhome_git-22.0
 
 opkg install luci-app-adguardhome_git-22.074.41762-a22cf4f_all.ipk
 rm luci-app-adguardhome_git-22.074.41762-a22cf4f_all.ipk
-rm /etc/config/AdGuardHome-opkg
