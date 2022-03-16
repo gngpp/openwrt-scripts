eed7d269a47d5dc310d9166e71328f6d89d46d09
Kardusen <verticle@foxmail.com>
2022-03-17 01:31:11 +0800

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 opkg update && opkg install zsh git git-http
-which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
 
 wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh && sh install.sh && rm install.sh
 source ~/.zshrc
+which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
+
