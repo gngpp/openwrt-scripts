4afe3fedf91d59740dd4fe627267ebb5573d5559
Kardusen <verticle@foxmail.com>
2022-03-16 16:55:17 +0800

    Update adbyby.sh

--- luci-app/adbyby.sh
+++ luci-app/adbyby.sh
@@ -4,6 +4,7 @@ wget https://op.supes.top/packages/aarch64_generic/luci-app-adbyby-plus_git-22.0
 wget https://op.supes.top/packages/aarch64_generic/adbyby_2.7-3_aarch64_generic.ipk
 
 opkg install adbyby_2.7-3_aarch64_generic.ipk
+rm /lib/upgrade/keep.d/adbyby
 opkg install luci-app-adbyby-plus_git-22.049.56376-f644175_all.ipk
 
 rm luci-app-adbyby-plus_git-22.049.56376-f644175_all.ipk
