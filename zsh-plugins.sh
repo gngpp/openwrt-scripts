97c92b9e0ebe4e46ed0c102d2ec75e76117422c9
zf1976 <verticle@foxmail.com>
2022-03-18 01:10:31 +0800

    feat: update

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 # zsh-syntax-highlighting
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-
 wget https://raw.githubusercontent.com/zf1976/openwrt-packages/main/neofetch.sh && sh neofetch.sh -i && rm neofetch.sh
 wget https://raw.githubusercontent.com/zf1976/packages/main/sysinfo.sh -P /etc
+
+echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 echo "neofetch" >> ~/.zshrc
 echo "bash /etc/sysinfo.sh" >> ~/.zshrc
 source ~/.zshrc
