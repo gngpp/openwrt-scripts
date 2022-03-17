51334c9ad64abfb01874888cf993d25f56ae9a34
Kardusen <verticle@foxmail.com>
2022-03-18 01:27:07 +0800

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sed -i '73d' ~/.zshrc
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
