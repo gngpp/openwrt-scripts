7289570c119e525b10483a9e6e5afd3bf6e33f14
Kardusen <verticle@foxmail.com>
2022-03-18 01:06:31 +0800

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -9,6 +9,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
+wget https://raw.githubusercontent.com/zf1976/openwrt-packages/main/neofetch.sh && sh neofetch.sh -i && rm neofetch.sh
 wget https://raw.githubusercontent.com/zf1976/packages/main/sysinfo.sh -P /etc
 echo "neofetch" >> ~/.zshrc
 echo "bash /etc/sysinfo.sh" >> ~/.zshrc
