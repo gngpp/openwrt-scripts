3ca4231e802f25f822e3f06c488ce493ea7e7574
Kardusen <verticle@foxmail.com>
2022-03-21 23:39:34 +0800

    Delete zsh.sh

--- zsh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-opkg update
-wget https://downloads.openwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/packages/zsh_5.8-1_aarch64_cortex-a53.ipk
-opkg install zsh_5.8-1_aarch64_cortex-a53.ipk
-rm ./zsh_5.8-1_aarch64_cortex-a53.ipk
-
-which zsh && sed -i -- 's:/bin/ash:'`which zsh`':g' /etc/passwd
-
