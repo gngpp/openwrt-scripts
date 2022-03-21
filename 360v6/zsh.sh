c85a732f93c5778771252eccd4b794a18718ef13
Kardusen <verticle@foxmail.com>
2022-03-21 23:40:37 +0800

    Update zsh.sh

--- 360v6/zsh.sh
+++ 360v6/zsh.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 opkg update
 wget https://downloads.openwrt.org/releases/21.02.1/packages/aarch64_cortex-a53/packages/zsh_5.8-1_aarch64_cortex-a53.ipk
 opkg install zsh_5.8-1_aarch64_cortex-a53.ipk
