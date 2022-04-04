5fc9e7bff10bcbb03ed50ba531ca6da9e9e0bf87
Kardusen <verticle@foxmail.com>
2022-04-04 21:09:58 +0800

    Update base.sh

--- R4S/luci-app/base.sh
+++ R4S/luci-app/base.sh
@@ -4,7 +4,7 @@ opkg update
 
 opkg install ttyd luci-i18n-ttyd-zh-cn luci-app-nlbwmon luci-i18n-nlbwmon-zh-cn luci-app-aria2 luci-i18n-aria2-zh-cn luci-app-openvpn luci-i18n-openvpn-zh-cn
 
-opkg install luci-app-uhttpd luci-i18n-uhttpd-zh-cn luci-app-minidlna luci-i18n-minidlna-zh-cn luci-i18n-samba4-zh-cn luci-app-samba4 luci-app-watchcat luci-i18n-watchcat-zh-cn luci-app-wol luci-i18n-wol-zh-cn luci-app-lxc luci-i18n-lxc-zh-cn
+opkg install luci-app-syncthing luci-i18n-syncthing-zh-cn luci-app-uhttpd luci-i18n-uhttpd-zh-cn luci-app-minidlna luci-i18n-minidlna-zh-cn luci-i18n-samba4-zh-cn luci-app-samba4 luci-app-watchcat luci-i18n-watchcat-zh-cn luci-app-wol luci-i18n-wol-zh-cn luci-app-lxc luci-i18n-lxc-zh-cn
 
 wget https://op.supes.top/packages/aarch64_generic/luci-app-accesscontrol_git-22.049.56376-f644175_all.ipk
 opkg install luci-app-accesscontrol_git-22.049.56376-f644175_all.ipk
