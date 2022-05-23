28ac639f3557ca13ef50ab457cd558e76fe97c91
gngpp <verticle@foxmail.com>
2022-05-23 19:57:09 +0800

    Update zsh-plugins.sh

--- 360v6/zsh-plugins.sh
+++ 360v6/zsh-plugins.sh
@@ -5,7 +5,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 wget https://raw.githubusercontent.com/gngpp/openwrt-packages/main/neofetch.sh && sh neofetch.sh -i && rm neofetch.sh
-wget https://raw.githubusercontent.com/gngpp/packages/main/360v6/sysinfo.sh -P /etc
+wget https://raw.githubusercontent.com/gngpp/openwrt-packages/main/360v6/sysinfo.sh -P /etc
 
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
