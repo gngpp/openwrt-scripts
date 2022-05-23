ca4ac03f36e888c8c8998e3287770437496efec4
gngpp <verticle@foxmail.com>
2022-05-23 20:49:08 +0800

    Update aliyundrive-fuse.sh

--- R4S/luci-app/aliyundrive-fuse.sh
+++ R4S/luci-app/aliyundrive-fuse.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ## r4s-arm64
-wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.9/aliyundrive-fuse_0.1.9-1_aarch64_generic.ipk
-wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.9/luci-app-aliyundrive-fuse_0.1.9_all.ipk
-wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.9/luci-i18n-aliyundrive-fuse-zh-cn_0.1.9-1_all.ipk
+wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.12/aliyundrive-fuse_0.1.12-1_aarch64_generic.ipk
+wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.12/luci-app-aliyundrive-fuse_0.1.12_all.ipk
+wget https://github.com/messense/aliyundrive-fuse/releases/download/v0.1.12/luci-i18n-aliyundrive-fuse-zh-cn_0.1.12-1_all.ipk
 
 opkg update
-opkg install aliyundrive-fuse_0.1.9-1_aarch64_generic.ipk
-opkg install luci-app-aliyundrive-fuse_0.1.9_all.ipk
-opkg install luci-i18n-aliyundrive-fuse-zh-cn_0.1.9-1_all.ipk
+opkg install aliyundrive-fuse_0.1.12-1_aarch64_generic.ipk
+opkg install luci-app-aliyundrive-fuse_0.1.12_all.ipk
+opkg install luci-i18n-aliyundrive-fuse-zh-cn_0.1.12-1_all.ipk
 
-rm aliyundrive-fuse_0.1.9-1_aarch64_generic.ipk
-rm luci-app-aliyundrive-fuse_0.1.9_all.ipk
-rm luci-i18n-aliyundrive-fuse-zh-cn_0.1.9-1_all.ipk
+rm aliyundrive-fuse_0.1.12-1_aarch64_generic.ipk
+rm luci-app-aliyundrive-fuse_0.1.12_all.ipk
+rm luci-i18n-aliyundrive-fuse-zh-cn_0.1.12-1_all.ipk
