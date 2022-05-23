77774d755b970ca6222b6ba7c88d0efa90d37ae7
gngpp <verticle@foxmail.com>
2022-05-23 20:44:36 +0800

    Update ftpserver.sh

--- R4S/luci-app/ftpserver.sh
+++ R4S/luci-app/ftpserver.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 opkg update
-wget https://op.supes.top/packages/aarch64_generic/vsftpd-alt_3.0.5-14_aarch64_generic.ipk
-wget https://op.supes.top/packages/aarch64_generic/luci-app-vsftpd_git-22.049.56376-f644175_all.ipk
+wget https://op.supes.top/packages/aarch64_generic/vsftpd-alt_3.0.5-13_aarch64_generic.ipk
+wget https://op.supes.top/packages/aarch64_generic/luci-app-vsftpd_git-22.138.56692-eb87df8_all.ipk
 
 opkg install vsftpd-alt_3.0.5-14_aarch64_generic.ipk
-opkg install luci-app-vsftpd_git-22.049.56376-f644175_all.ipk
+opkg install luci-app-vsftpd_git-22.138.56692-eb87df8_all.ipk
 
 rm vsftpd-alt_3.0.5-14_aarch64_generic.ipk
-rm luci-app-vsftpd_git-22.049.56376-f644175_all.ipk
+rm luci-app-vsftpd_git-22.138.56692-eb87df8_all.ipk
