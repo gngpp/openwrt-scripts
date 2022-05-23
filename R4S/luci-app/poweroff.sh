dee8378f8d926ff0d253f723cd3d21786be9e4d1
gngpp <verticle@foxmail.com>
2022-05-23 20:51:48 +0800

    Update poweroff.sh

--- R4S/luci-app/poweroff.sh
+++ R4S/luci-app/poweroff.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://op.supes.top/packages/aarch64_generic/luci-app-poweroff_git-21.361.69956-1fc1d61_all.ipk 
-opkg install luci-app-poweroff_git-21.361.69956-1fc1d61_all.ipk
-rm luci-app-poweroff_git-21.361.69956-1fc1d61_all.ipk
+wget https://op.supes.top/packages/aarch64_generic/luci-app-poweroff_git-22.138.56692-eb87df8_all.ipk
+opkg install luci-app-poweroff_git-22.138.56692-eb87df8_all.ipk
+rm luci-app-poweroff_git-22.138.56692-eb87df8_all.ipk
