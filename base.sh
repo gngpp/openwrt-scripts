02cc0f62bced9893d1f534a356df61c7e3f3aaba
gngpp <verticle@foxmail.com>
2022-05-23 19:22:07 +0800

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # base
-opkg update && opkg install vim rsync cfdisk git git-http usbutils pciutils lsof coreutils-nohup speedtest-go
+opkg update && opkg install vim rsync cfdisk git git-http usbutils pciutils lsof coreutils-nohup
 echo "set number" >> /usr/share/vim/vimrc
 echo "set rnu" >> /usr/share/vim/vimrc
