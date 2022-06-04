cc23d60ef2f719eb57cd4bfa4475f9f70720f4e8
gngpp <verticle@foxmail.com>
2022-06-04 19:49:19 +0800

    Update base.sh

--- R4S/luci-app/base.sh
+++ R4S/luci-app/base.sh
@@ -8,6 +8,6 @@ opkg install luci-app-syncthing luci-i18n-syncthing-zh-cn luci-app-uhttpd luci-i
 
 opkg install luci-app-hd-idle luci-i18n-hd-idle-zh-cn luci-app-gowebdav luci-i18n-gowebdav-zh-cn luci-app-advancedsetting luci-i18n-advancedsetting-zh-cn luci-app-omcproxy luci-i18n-omcproxy-zh-cn luci-app-verysync luci-i18n-verysync-zh-cn
 
-opkg install luci-app-rclone luci-i18n-rclone-zh-cn luci-app-filebrowser luci-app-speederv2 luci-i18n-speederv2-zh-cn
+opkg install luci-app-rclone luci-i18n-rclone-zh-cn luci-app-filebrowser luci-app-speederv2 luci-i18n-speederv2-zh-cn netdata luci-app-netdata luci-i18n-netdata-zh-cn
 
-opkg install adbyby luci-app-adbyby-plus luci-i18n-adbyby-plus-zh-cn gost	luci-app-gost luci-i18n-gost-zh-cn luci-app-filetransfer luci-i18n-filetransfer-zh-cn	
+opkg install adbyby luci-app-adbyby-plus luci-i18n-adbyby-plus-zh-cn gost	luci-app-gost luci-i18n-gost-zh-cn luci-app-filetransfer luci-i18n-filetransfer-zh-cn
