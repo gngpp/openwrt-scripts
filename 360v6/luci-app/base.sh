fe437951805c8f43e2f88cb843238b848f90e9d4
gngpp <verticle@foxmail.com>
2022-06-27 11:14:14 +0800

    Update base.sh

--- 360v6/luci-app/base.sh
+++ 360v6/luci-app/base.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 opkg update
-opkg install luci-app-uhttpd luci-i18n-uhttpd-zh-cn luci-app-nlbwmon luci-i18n-nlbwmon-zh-cn
+opkg install luci-app-uhttpd luci-i18n-uhttpd-zh-cn luci-app-nlbwmon luci-i18n-nlbwmon-zh-cn frpc frps luci-app-frpc luci-app-frps luci-i18n-frpc-zh-cn luci-i18n-frps-zh-cn
