44bacf6efba6c334efb9bfea7984e7f6e1c0f163
gngpp <1596863112@qq.com>
2023-02-24 20:10:51 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cat /etc/opkg/distfeeds.conf
+
+cat > /etc/opkg/distfeeds.conf <<EOF
+src/gz openwrt_base https://openwrt.888913.gq/aarch64_cortex-a53/base
+src/gz openwrt_luci https://openwrt.888913.gq/aarch64_cortex-a53/luci
+src/gz openwrt_packages https://openwrt.888913.gq/aarch64_cortex-a53/packages
+src/gz openwrt_routing https://openwrt.888913.gq/aarch64_cortex-a53/routing
+src/gz openwrt_telephony https://openwrt.888913.gq/aarch64_cortex-a53/telephony
+EOF
+
+cat /etc/opkg/distfeeds.conf
+
+opkg update && opkg install luci-app-passwall
